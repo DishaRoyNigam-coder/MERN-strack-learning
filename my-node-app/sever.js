ebe70bde3b48ca9df7f26dfdd3439f1b3c8aa175
Disha Nigam <nigamdish23@gmail.com>
2026-08-07 22:25:58 +0530

    Day 46: First React app with Vite

---
+++ my-node-app/sever.js
@@ -0,0 +1,13 @@
+// server.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Hello, Node.js!</h1><p>Server is running with nodemon.</p>');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+    console.log(`🔄 Nodemon is watching for changes........`);
+});
